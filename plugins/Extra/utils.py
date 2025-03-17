7ab6f20526402bcf689aa37614346e67fe67e7e2
super7usr <glenna.lakin@blenched.com>
2025-03-17 21:50:02 +0100

    Update utils.py

--- plugins/Extra/utils.py
+++ plugins/Extra/utils.py
@@ -25,8 +25,8 @@ async def progress_for_pyrogram(current, total, ud_type, message, start):
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
         progress = "{0}{1}".format(
-            ''.join(["█" for i in range(math.floor(percentage / 5))]),
-            ''.join(["░" for i in range(20 - math.floor(percentage / 5))]))
+            ''.join(["🔵" for i in range(math.floor(percentage / 5))]),
+            ''.join(["⚪" for i in range(20 - math.floor(percentage / 5))]))
             
         tmp = progress + script.PROGRESS_BAR.format( 
             round(percentage, 2),
