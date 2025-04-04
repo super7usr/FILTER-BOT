de1842225fd0685e3266cec5fa84a6d4eb3687b3
super7usr <glenna.lakin@blenched.com>
2025-04-05 00:27:43 +0100

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -112,6 +112,41 @@ async def get_search_results(chat_id, query, file_type=None, max_results=10, off
 
     return files, next_offset, total_results
 
+async def get_all_search_results(query, file_type=None, offset=0, filter=False):
+    """For given query return (results, next_offset)"""
+    
+    query = query.strip()
+    if not query:
+        raw_pattern = '.'
+    elif ' ' not in query:
+        raw_pattern = r'(\b|[\.\+\-_])' + query + r'(\b|[\.\+\-_])'
+    else:
+        raw_pattern = query.replace(' ', r'.*[\s\.\+\-_]') 
+    try:
+        regex = re.compile(raw_pattern, flags=re.IGNORECASE)
+    except:
+        regex = query
+    filter = {'file_name': regex}
+    files = []
+    if MULTIPLE_DATABASE:
+        cursor1 = col.find(filter).sort('$natural', -1).skip(offset)
+        cursor2 = sec_col.find(filter).sort('$natural', -1).skip(offset)
+        
+        for file in cursor1:
+            files.append(file)
+        for file in cursor2:
+            files.append(file)
+    else:
+        cursor = col.find(filter).sort('$natural', -1).skip(offset).limit(max_results)
+        
+        for file in cursor:
+            files.append(file)
+
+    total_results = col.count_documents(filter) if not MULTIPLE_DATABASE else (col.count_documents(filter) + sec_col.count_documents(filter))
+    next_offset = "" if offset >= total_results else offset
+
+    return files, next_offset, total_results
+    
 async def get_bad_files(query, file_type=None, use_filter=False):
     """For given query return (results, next_offset)"""
     query = query.strip()
