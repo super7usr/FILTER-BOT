6a7fea6cee995107f19f6f6edeaac16f747c36b5
super7usr <glenna.lakin@blenched.com>
2025-04-05 00:34:37 +0100

    Update app.py

--- TechVJ/template/app.py
+++ TechVJ/template/app.py
@@ -5,7 +5,7 @@ import humanize
 import secrets
 import json
 from utils import temp
-from database.ia_filterdb import get_search_results as get_file_ids_from_imdb_search, get_file_details
+from database.ia_filterdb import get_all_search_results
 import os
 
 ia = Cinemagoer()
@@ -20,8 +20,8 @@ async def get_imdb_data(search_query):
         return movie
     return None
 
-async def get_cap(file_ids, search):
-    cap = f"<b>Tʜᴇ Rᴇꜱᴜʟᴛꜱ Fᴏʀ ☞ {search}\n\n</b>"
+async def get_cap(file_ids, search_query):
+    cap = f"<b>Tʜᴇ Rᴇꜱᴜʟᴛꜱ Fᴏʀ ☞ {search_query}\n\n</b>"
     cap += "<b>\n\n<u>🍿 Your Movie Files 👇</u></b>\n\n"
 
     for file_id in file_ids:
@@ -42,17 +42,11 @@ async def movie_search(request):
     if not search_query:
         return web.Response(text="Please provide a movie query using '?q=movie_name'", status=400)
 
-    imdb_data = await get_imdb_data(search_query)
+    db_data = await get_all_search_results(search_query)
 
-    if imdb_data:
-        file_ids = await get_file_ids_from_imdb_search(imdb_data["imdb_id"])
-
-        if file_ids:
-            cap = await get_cap(file_ids, search_query)
-            return web.Response(text=cap, content_type="text/html")
-        else:
-            return web.Response(text="No files found for this movie.", content_type="text/html")
-
+    if file_ids:
+        cap = await get_cap(file_ids, search_query)
+        return web.Response(text=cap, content_type="text/html")
     else:
         return web.Response(
             text=f"No movie found with the name '{search_query}'. It might not be released or added yet.",
